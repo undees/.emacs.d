91a5d465d63187a6c61a7cb9fea25d49fe589e19
Erin Dees <undees@gmail.com>
2008-09-23 17:42:31 -0700

    Minor startup annoyances

--- init.el
+++ init.el
@@ -143,6 +143,7 @@ is a comment, uncomment."
   ;; If there is more than one, they won't work right.
  '(ecb-options-version "2.32")
  '(ecb-primary-secondary-mouse-buttons (quote mouse-1--mouse-2))
- '(ecb-tar-setup (quote cons))
+ '(ecb-tar-setup (quote ("/bin/tar" . other)))
  '(ecb-tip-of-the-day nil)
+ '(inhibit-startup-screen t)
  '(org-agenda-files (directory-files "~/org" t "\\.org$")))
