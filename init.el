307ba6c8dfb68b95f156505fa95b263a1df7d796
Erin Dees <undees@gmail.com>
2008-09-23 17:41:47 -0700

    Sensible temp files

--- init.el
+++ init.el
@@ -37,6 +37,21 @@
 
 ;;;; General text editing
 
+;; Put backup files somewhere sensible
+;; http://amitp.blogspot.com/2007/03/emacs-move-autosave-and-backup-files.html
+(defvar user-temporary-file-directory
+  (concat temporary-file-directory user-login-name "/"))
+(make-directory user-temporary-file-directory t)
+(setq backup-by-copying t)
+(setq backup-directory-alist
+      `(("." . ,user-temporary-file-directory)
+        (,tramp-file-name-regexp nil)))
+(setq auto-save-list-file-prefix
+      (concat user-temporary-file-directory ".auto-saves-"))
+(setq auto-save-file-name-transforms
+      `((".*" ,user-temporary-file-directory t)))
+
+
 ;; So I can see what I'm highlighting
 (transient-mark-mode)
 
