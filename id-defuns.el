e2f094cea5ce8120182d2d60937e35b003969c46
Ian Dees <undees@gmail.com>
2009-02-26 16:28:58 -0800

    Keyboard tweaks

--- id-defuns.el
+++ id-defuns.el
@@ -2,27 +2,6 @@
 ;;
 ;; Thanks to the Emacs Starter Kit
 
-;; TextMate-like commenting
-;; http://paste.lisp.org/display/42657
-(defun comment-or-uncomment-line (&optional lines)
-  "Comment current line. Argument gives the number of lines
-forward to comment"
-  (interactive "P")
-  (comment-or-uncomment-region
-   (line-beginning-position)
-   (line-end-position lines)))
-
-(defun comment-or-uncomment-region-or-line (&optional lines)
-  "If the line or region is not a comment, comments region
-if mark is active, line otherwise. If the line or region
-is a comment, uncomment."
-  (interactive "P")
-  (if mark-active
-      (if (< (mark) (point))
-          (comment-or-uncomment-region (mark) (point))
-	(comment-or-uncomment-region (point) (mark)))
-    (comment-or-uncomment-line lines)))
-
 (defun coding-hook ()
   "Enable things that are convenient across all coding buffers."
   (set (make-local-variable 'comment-auto-fill-only-comments) t)
