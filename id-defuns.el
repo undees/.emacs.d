ba5ccec80767004cc3325dd06f5287f6ea090b69
Ian Dees <undees@gmail.com>
2009-05-04 00:50:30 -0700

    Coding hooks

--- id-defuns.el
+++ id-defuns.el
@@ -36,15 +36,15 @@
                              (cond
                               ((and (listp symbol) (imenu--subalist-p symbol))
                                (addsymbols symbol))
-                              
+
                               ((listp symbol)
                                (setq name (car symbol))
                                (setq position (cdr symbol)))
-                              
+
                               ((stringp symbol)
                                (setq name symbol)
                                (setq position (get-text-property 1 'org-imenu-marker symbol))))
-                             
+
                              (unless (or (null position) (null name))
                                (add-to-list 'symbol-names name)
                                (add-to-list 'name-and-pos (cons name position))))))))
@@ -59,6 +59,22 @@
   (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
     (when file
       (find-file file))))
+
+; http://www.bretthutley.com/emacs/opening-a-cobjective-cc-header-file-in-emacs
+(defun id-choose-header-mode ()
+  "Decide whether a .h file is C, C++, or Objective-C"
+  (interactive)
+  (if (string-equal (substring (buffer-file-name) -2) ".h")
+      (progn
+        ;; OK, we got a .h file, if a .m file exists we'll assume it's
+        ; an objective c file. Otherwise, we'll look for a .cpp file.
+        (let ((dot-m-file (concat (substring (buffer-file-name) 0 -1) "m"))
+              (dot-cpp-file (concat (substring (buffer-file-name) 0 -1) "cpp")))
+          (if (file-exists-p dot-m-file)
+              (progn
+                (objc-mode))
+            (if (file-exists-p dot-cpp-file)
+                (c++-mode)))))))
 
 (provide 'id-defuns)
 ;;; id-defuns.el ends here
