3333f30471112764ff4b447ff10593f27812480a
Erin Dees <undees@gmail.com>
2019-02-12 11:46:01 -0800

    Update for emacs-mac 26.1

--- init.el
+++ init.el
@@ -5,6 +5,13 @@
 
 ;; Find it!
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
